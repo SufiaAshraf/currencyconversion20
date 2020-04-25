7e943c5f83dc8a7a9f63e2b6b5656406f02b7e81
Sufia Ashraf <42400674+SufiaAshraf@users.noreply.github.com>
2020-04-25 15:45:50 +0530

    Update app.js
    Make it better

--- app.js
+++ app.js
@@ -5,13 +5,8 @@ app.set("view engine" , "ejs");
 app.use(express.json());
 
 // Setting Local Host
-    const http = require('http');
     const hostname = '127.0.0.1';
     const port = 3000;
-    const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.end();
-});   
 
 // Landing page
 app.get("/", function(req, res){
