039398253aced60b047955bdd7dc7a77401a108e
Sufia Ashraf <42400674+SufiaAshraf@users.noreply.github.com>
2020-04-26 02:30:22 +0530

    better

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 var express = require('express'),
     app = express();
-var http = require('http');
 app.set("view engine" , "ejs");
 app.use(express.json());
 app.use(express.static("views"))
 // Setting Local Host
     // const hostname = '0.0.0.0';
-    const port =  process.env.port || 3000
-    const server = http.createServer((req, res) => {
-        res.statusCode = 200;
-    });
+    // const port = 3000;
 
-// // Landing page
+// Landing page
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 // Port
-app.listen(port, () => {
-    console.log(`Server running at ` +port);
-});
+app.listen(process.env.port || 3000,
+    () => console.log(`Server running`));
       
